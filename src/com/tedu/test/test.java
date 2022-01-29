8621e084ea705dfb30a3c65416e70b5412fc2d5e
yu <82295304@qq.com>
2022-01-29 20:40:54 +0800

    Initial commit

---
+++ src/com/tedu/test/test.java
@@ -0,0 +1,16 @@
+package com.tedu.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class test {
+    //注释
+
+    public static void main(String[]args){
+        System.out.println("helloword111111111");
+        List list = new ArrayList();
+    }
+    public void add(){
+
+    }
+}
---
+++ src/hello.java
@@ -0,0 +1,32 @@
+import com.sun.org.apache.xpath.internal.operations.String;
+
+import java.util.ArrayList;
+
+/**
+ * @author xiao_Y
+ * @date 2022/1/29 0029 -17:34
+ */
+public class hello {
+    //单行
+    /**
+     * 多行
+    */
+    public  static  final  see(){
+        //11
+        //55555
+        String us = new String("ss");
+        String s = new String();
+        for (int i = 0; i < ; i++) {
+
+        }
+        for (Object o : ) {
+            
+        }
+        System.out.println(s);
+
+    }
+
+    public static void main(String[] args) {
+        String da = new String();
+    }
+}
