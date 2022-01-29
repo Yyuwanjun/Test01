40c86948433a659c33bf9a7dc368895e701fe395
yu <82295304@qq.com>
2022-01-29 21:15:35 +0800

    aaa

---
+++ src/com/tedu/test/demo007.java
@@ -0,0 +1,12 @@
+package com.tedu.test;
+
+/**
+ * @author xiao_Y
+ * @date 2022/1/29 0029 -20:57
+ */
+public class demo007 {
+    public static void main(String[] args) {
+
+        System.out.println("aaa");
+    }
+}
