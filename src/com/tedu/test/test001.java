2fe6b753bf74270d06693c7f8c5f5487bb27f4f2
yu <82295304@qq.com>
2022-01-29 21:22:46 +0800

    test01

---
+++ src/com/tedu/test/test001.java
@@ -0,0 +1,11 @@
+package com.tedu.test;
+
+/**
+ * @author xiao_Y
+ * @date 2022/1/29 0029 -21:22
+ */
+public class test001 {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
