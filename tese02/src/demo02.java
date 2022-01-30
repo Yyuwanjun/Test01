8bdebc368c4e04b2ab6333a4129b10e385e0dcba
yu <82295304@qq.com>
2022-01-30 17:49:14 +0800

    测试

--- tese02/src/demo02.java
+++ tese02/src/demo02.java
@@ -1,3 +1,5 @@
+import com.sun.org.apache.xpath.internal.operations.String;
+
 /**
  * @author xiao_Y
  * @date 2022/1/30 0030 -15:59
@@ -5,5 +7,6 @@
 public class demo02 {
     public static void main(String[] args) {
         System.out.println("bbbbbbb");
+        String aa = new String("dddddddd");
     }
 }
